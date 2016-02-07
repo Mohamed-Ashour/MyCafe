e34d848071b7cdfdce31e9ba6598f2121bee87c6
mohamed-ashour <e.mohamed77@gmail.com>
2016-02-08 01:32:13 +0200

    added jquery and bootstrap files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="js/jquery-2.2.0.min.js"></script>
+</head>
+<body>
+	<div class="container">
+
+	</div>
+</body>
+</html>
